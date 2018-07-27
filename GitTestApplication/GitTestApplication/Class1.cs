83da51c9e279af37888de73c78ba37c51594635b
ligenying <ligenyingsr@163.com>
2018-07-27 13:29:31 +0300

    first commit

---
+++ GitTestApplication/GitTestApplication/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestApplication
+{
+    public class Class1
+    {
+        public string name = "aa";
+        public string sex = "";
+        public int age=23;
+    }
+}
